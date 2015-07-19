642c73e087b56adec6384f93c9bd6c0c5736553b
Philip Hutchins <philip@bitpay.com>
2015-07-19 15:45:36 -0400

    pushign tests

--- spec/server/socketServer.spec.js
+++ spec/server/socketServer.spec.js
@@ -21,7 +21,6 @@ var Room = require('../../models/room');
 var configHttps = require('../../config/https');
 
 var SocketServer = require('../../socketServer');
-var socketServer = new SocketServer();
 
 
 // Stubbed objects
@@ -36,6 +35,12 @@ var testUser = new User({
     currentRooms: [],
     rooms: []
   }
+});
+
+var validUserData = ({
+  userName: 'testUser',
+  publicKey: '-----BEGIN PGP PUBLIC KEY BLOCK-----\nVersion: OpenPGP.js v1.0.1\nComment: http://openpgpjs.org\n\nxsBNBFWoMlQBCADfMUcy8Z31SvX/UbwzE8afo1mSuBEQGd7UTkcwEbmWOScT\nZu66tjAgGfWLpcot+A1eVA8mPiHQlgsLzMJu1AKm+u2ZNY/lzBgmcX/TsyK+\ng5mK3kzfPWUPpvt1+fgBwEe8UGUu9I0xk80MNqe7bJO5O2VULe+ofbCuc/L0\ngY4Fh8CfkEAYnOTPKA1tEb/7P9cu5YXJKJecbpwDJ/pZU2cAM4/OnRxATz0G\nwVUaWBtjRr+Bk+FulZQjJ0TqlFdPZck+37MpCrYTcOdUieA0yA1s6Ma0RNh1\nN/c95CXrawLDUWELIyg6AF5XUxbLKF0vDVV4l8k/I/tB/ZZtvEeMQerVABEB\nAAHNBmZsaXAxMsLAcgQQAQgAJgUCVagyWgYLCQgHAwIJECvy/wuE+RVwBBUI\nAgoDFgIBAhsDAh4BAACU3ggAoEtxanrqzrRikoapSLdZ23pdh2xf2NpTRUb6\nuG7BcaF6tYm3gXgOYeCcWNeSliO6whN/IzvZnsLpKEWgqz10YvTbs9nPmOKH\n18WEWKlTXhETDEU8AWGt/RvYjYx5msDoVyX35UOto6zKgV/aOWL8d86IDpPH\nfIe3S6/gm3alXeMmpGkzZUUR8J3kjHL/P+3f5ywN+YL7LO5iT2dn5MlSV6CN\n5spWcZH52HfhZWfZwb/KKcBG94yaUVAvDgihUa/G2xufy0JbMrS97AdjPK/9\nRopcGHc7nljLdNA41RPq8op34KzlDb9fEdD1a0KtHSsX+KPEmcLaEbCv+5Gq\nZkQ/qs7ATQRVqDJUAQgA5/3VSPG774BHfTyy0umwo/5u67V+sugAKn1rH79+\n7zRiuISkb29OKkHE29otuUdLJq7FhfxxadjSdXuMXYVlrTCASlNsd3mVgYFP\n/JxmgbvwO7vaaui40b7RWmbkQxYZU3ChsP3EMbA29VzBDQ2er3GEtSA1/m5O\n6fdk/Fuf9cZ8ta72MLqqCAf4gZa9ztBnVf6LdvITa9h8DSPhP2tq2M8/7iMQ\nnOVCFS51GK4GAP6d0OSPrkrtJF4l70+DNVUNIR6Gtm2VRLkMgOvVtXV8ejSy\nOqpjbDoxi3R5PQOqqGcWGHgpsDfid8zEeJNDZvwy8YUA+WXTeQ2nPtrn3mDs\nuQARAQABwsBfBBgBCAATBQJVqDJeCRAr8v8LhPkVcAIbDAAAnc8IAKvC36wa\nBxZ0xP0Cm/7/vLabgaL0EeI7juWCDejg/bSc6SQjtJ9Tn91yCJ1Y2e3vZNFB\nYmV/GhZxadZNYTJPJg3bVl1a1iQNlnBa9JSaEJ07glgOtTyNlChzF3uTA5rr\nplRGL8cF4OfuI3TFV2QdHXsPYgNE0rYQI9kCbDcTSKBcD4WY7wYhQCCNs/v6\nDCYf60hcjZVCNskTsnLqTFIxTSAUag5KaBnekyThUd94raO6HjuW+A1eDRFR\nzZpxc7OFmGB0FzRQx3GhQvla4xevaoQjU09aD9Lstkc9Kc1s+JuGGF14zOu1\n2ypvMLDaWnmVFM7VW99sVHY0y6rUmjW3D5k=\n=nKRV\n-----END PGP PUBLIC KEY BLOCK-----',
+  email: 'test@testmail.com'
 });
 
 var testPublicRoom = new Room({
@@ -53,6 +58,7 @@ testPublicRoom._members.push(testUser);
 testPublicRoom._admins.push(testUser);
 
 describe('Sanatize room for client', function() {
+  var socketServer = new SocketServer();
   it('should convert _members object to array of usernames', function() {
     var correctlySanatizedRoom = ({
       name : 'testroom',
@@ -73,6 +79,7 @@ describe('Sanatize room for client', function() {
 });
 
 describe('Get default room', function() {
+  var socketServer = new SocketServer();
   it('should return a room', function() {
     socketServer.getDefaultRoom(function(defaultRoom) {
       expect(typeof defaultRoom == mongoose.Schema('Room')).toBe(true);
@@ -86,55 +93,33 @@ describe('Get default room', function() {
 });
 
 describe('Chat server', function() {
-  var socket;
-
-  beforeEach(function(done) {
-    // Setup
-    socket = io.connect(socketURL, options);
-    socket.on('connect', function() {
-      console.log('worked...');
-      done();
-    });
-    socket.on('disconnect', function() {
-      console.log('disconnected...');
-    })
-  });
-
-  afterEach(function(done) {
-    // Cleanup
-    if(socket.connected) {
-      console.log('disconnecting...');
-      socket.disconnect();
-    } else {
-      // There will not be a connection unless you have done() in beforeEach, socket.on('connect'...)
-      console.log('no connection to break...');
-    }
-    done();
-  });
-
+  var socket, socketEmitData, socketOnData, emitSpy, onSpy;
+  //var spy = jasmine.createSpy('spy');
+  var socketServer;
   describe('authentication', function() {
-    var data = ({
-      userName: 'testUser',
-      publicKey: '-----BEGIN PGP PUBLIC KEY BLOCK-----\nVersion: OpenPGP.js v1.0.1\nComment: http://openpgpjs.org\n\nxsBNBFWoMlQBCADfMUcy8Z31SvX/UbwzE8afo1mSuBEQGd7UTkcwEbmWOScT\nZu66tjAgGfWLpcot+A1eVA8mPiHQlgsLzMJu1AKm+u2ZNY/lzBgmcX/TsyK+\ng5mK3kzfPWUPpvt1+fgBwEe8UGUu9I0xk80MNqe7bJO5O2VULe+ofbCuc/L0\ngY4Fh8CfkEAYnOTPKA1tEb/7P9cu5YXJKJecbpwDJ/pZU2cAM4/OnRxATz0G\nwVUaWBtjRr+Bk+FulZQjJ0TqlFdPZck+37MpCrYTcOdUieA0yA1s6Ma0RNh1\nN/c95CXrawLDUWELIyg6AF5XUxbLKF0vDVV4l8k/I/tB/ZZtvEeMQerVABEB\nAAHNBmZsaXAxMsLAcgQQAQgAJgUCVagyWgYLCQgHAwIJECvy/wuE+RVwBBUI\nAgoDFgIBAhsDAh4BAACU3ggAoEtxanrqzrRikoapSLdZ23pdh2xf2NpTRUb6\nuG7BcaF6tYm3gXgOYeCcWNeSliO6whN/IzvZnsLpKEWgqz10YvTbs9nPmOKH\n18WEWKlTXhETDEU8AWGt/RvYjYx5msDoVyX35UOto6zKgV/aOWL8d86IDpPH\nfIe3S6/gm3alXeMmpGkzZUUR8J3kjHL/P+3f5ywN+YL7LO5iT2dn5MlSV6CN\n5spWcZH52HfhZWfZwb/KKcBG94yaUVAvDgihUa/G2xufy0JbMrS97AdjPK/9\nRopcGHc7nljLdNA41RPq8op34KzlDb9fEdD1a0KtHSsX+KPEmcLaEbCv+5Gq\nZkQ/qs7ATQRVqDJUAQgA5/3VSPG774BHfTyy0umwo/5u67V+sugAKn1rH79+\n7zRiuISkb29OKkHE29otuUdLJq7FhfxxadjSdXuMXYVlrTCASlNsd3mVgYFP\n/JxmgbvwO7vaaui40b7RWmbkQxYZU3ChsP3EMbA29VzBDQ2er3GEtSA1/m5O\n6fdk/Fuf9cZ8ta72MLqqCAf4gZa9ztBnVf6LdvITa9h8DSPhP2tq2M8/7iMQ\nnOVCFS51GK4GAP6d0OSPrkrtJF4l70+DNVUNIR6Gtm2VRLkMgOvVtXV8ejSy\nOqpjbDoxi3R5PQOqqGcWGHgpsDfid8zEeJNDZvwy8YUA+WXTeQ2nPtrn3mDs\nuQARAQABwsBfBBgBCAATBQJVqDJeCRAr8v8LhPkVcAIbDAAAnc8IAKvC36wa\nBxZ0xP0Cm/7/vLabgaL0EeI7juWCDejg/bSc6SQjtJ9Tn91yCJ1Y2e3vZNFB\nYmV/GhZxadZNYTJPJg3bVl1a1iQNlnBa9JSaEJ07glgOtTyNlChzF3uTA5rr\nplRGL8cF4OfuI3TFV2QdHXsPYgNE0rYQI9kCbDcTSKBcD4WY7wYhQCCNs/v6\nDCYf60hcjZVCNskTsnLqTFIxTSAUag5KaBnekyThUd94raO6HjuW+A1eDRFR\nzZpxc7OFmGB0FzRQx3GhQvla4xevaoQjU09aD9Lstkc9Kc1s+JuGGF14zOu1\n2ypvMLDaWnmVFM7VW99sVHY0y6rUmjW3D5k=\n=nKRV\n-----END PGP PUBLIC KEY BLOCK-----',
-      email: 'test@testmail.com'
-    });
+    beforeEach(function() {
+      emitSpy = createSpy('emitSpy');
+      onSpy = createSpy('onSpy');
+      console.log("running before each");
+      socket = {
+        emit : emitSpy,
+        on : onSpy
+      };
 
-    it('should be successful with a valid key', function(done) {
-      socket.emit('authenticate', data);
-      socket.on('authenticated', function(data) {
-        var message = data.message;
-        expect(message).toMatch('ok');
-        done();
-      })
+      socketServer = new SocketServer(socket);
     })
 
-    it('should send a list of rooms with membership to the client', function(done) {
-      socket.emit('authenticate', data);
-      socket.on('membershipUpdate', function(data) {
-        var rooms = data.rooms;
-        expect(typeof rooms == 'array').toBe(true);
-        done();
-      })
+    afterEach(function() {
+    })
+
+    it('should be successful with a valid key', function() {
+      socketServer.authenticate(validUserData);
+      expect(emitSpy).toHaveBeenCalledWith({ 'message': 'ok'});
+    });
+
+    it('should send a list of rooms with membership to the client', function() {
+      socketServer.authenticate(validUserData);
+      expect(onSpy).toHaveBeenCalledWith({ bleh: 'hi' });
     })
   })
 });

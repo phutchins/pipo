07544a3a47872d90c39d700922c81fed9852cf18
Philip Hutchins <philip@bitpay.com>
2015-09-08 08:44:40 -0400

    WIP: Moving notifications to its own lib

--- public/js/chatManager.js
+++ public/js/chatManager.js
@@ -1062,60 +1062,6 @@ ChatManager.populateUserPopup = function populateUserPopup(data) {
 };
 
 
-// Sends a notification that expires after a timeout. If timeout = 0 it does not expire
-ChatManager.sendNotification = function sendNotification(image, title, message, timeout, showOnFocus) {
-  this.getNotifyPermissions(function(permission) {
-    if (permission) {
-      console.log("[NOTIFICATION] Attempting to display notification");
-      // Default values for optional params
-      timeout = (typeof timeout !== 'undefined') ? timeout : 0;
-      showOnFocus = (typeof showOnFocus !== 'undefined') ? showOnFocus : false;
-      // Check if the browser window is focused
-      var isWindowFocused = document.querySelector(":focus") !== null;
-      // Check if we should send the notification based on the showOnFocus parameter
-      var shouldNotify = !isWindowFocused || isWindowFocused && showOnFocus;
-      console.log("[NOTIFICATION] shouldNotify is "+shouldNotify);
-      if (shouldNotify) {
-        console.log("[NOTIFCATION] Sending notification now...");
-        var notification = new Notification(title, { body: message });
-        if (timeout > 0) {
-          // Hide the notification after the timeout
-          setTimeout(function(){
-            notification.close();
-          }, timeout);
-        }
-      }
-    } else {
-      console.log("Don't have permission to display notification");
-    }
-  });
-};
-
-ChatManager.getNotifyPermissions = function getNotifyPermissions(callback) {
-  // check for notification compatibility
-  if(!window.Notification) {
-    // if browser version is unsupported, be silent
-    return callback(false);
-  }
-  // log current permission level
-  console.log(Notification.permission);
-  // if the user has not been asked to grant or deny notifications from this domain
-  if(Notification.permission === 'default') {
-    Notification.requestPermission(function() {
-      // callback this function once a permission level has been set
-      return callback(true);
-    });
-  }
-  // if the user has granted permission for this domain to send notifications
-  else if(Notification.permission === 'granted') {
-    return callback(true);
-  }
-  // if the user does not want notifications to come from this domain
-  else if(Notification.permission === 'denied') {
-    return callback(false);
-  }
-};
-
 ChatManager.enableChat = function enableChat(room, encryptionScheme) {
   var self = this;
 
---
+++ public/js/notification.js
@@ -0,0 +1,63 @@
+var Notification = {};
+
+Notification.init = function init() {
+  this.getPermission(function(permission) {
+    if (permission) {
+      console.log("Have notification permissions!");
+    }
+  })
+}
+
+Notification.getPermission = function getPermission(callback) {
+  // check for notification compatibility
+  if(!window.Notification) {
+    // if browser version is unsupported, be silent
+    return callback(false);
+  }
+  // log current permission level
+  console.log(Notification.permission);
+  // if the user has not been asked to grant or deny notifications from this domain
+  if(Notification.permission === 'default') {
+    Notification.requestPermission(function() {
+      // callback this function once a permission level has been set
+      return callback(true);
+    });
+  }
+  // if the user has granted permission for this domain to send notifications
+  else if(Notification.permission === 'granted') {
+    return callback(true);
+  }
+  // if the user does not want notifications to come from this domain
+  else if(Notification.permission === 'denied') {
+    return callback(false);
+  }
+};
+
+// Sends a notification that expires after a timeout. If timeout = 0 it does not expire
+Notification.send = function send(image, title, message, timeout, showOnFocus) {
+  this.getNotifyPermissions(function(permission) {
+    if (permission) {
+      console.log("[NOTIFICATION] Attempting to display notification");
+      // Default values for optional params
+      timeout = (typeof timeout !== 'undefined') ? timeout : 0;
+      showOnFocus = (typeof showOnFocus !== 'undefined') ? showOnFocus : false;
+      // Check if the browser window is focused
+      var isWindowFocused = document.querySelector(":focus") !== null;
+      // Check if we should send the notification based on the showOnFocus parameter
+      var shouldNotify = !isWindowFocused || isWindowFocused && showOnFocus;
+      console.log("[NOTIFICATION] shouldNotify is "+shouldNotify);
+      if (shouldNotify) {
+        console.log("[NOTIFCATION] Sending notification now...");
+        var notification = new Notification(title, { body: message });
+        if (timeout > 0) {
+          // Hide the notification after the timeout
+          setTimeout(function(){
+            notification.close();
+          }, timeout);
+        }
+      }
+    } else {
+      console.log("Don't have permission to display notification");
+    }
+  });
+};

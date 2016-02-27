c354c2c05fe6a0d6686101d721efc4d6ef412ff9
Philip Hutchins <philip@yikyakapp.com>
2016-02-27 12:12:55 -0500

    WIP: adding logging for server vs electron

--- main.js
+++ main.js
@@ -3,8 +3,10 @@
 // Detect whether we are running inside of electron or nodejs
 if (process.versions['electron']) {
   // Running inside of electron so run the client
+  console.log("Starting as Electron");
   require('./public/client.js');
 } else {
   // Running in node (or something else, should be more specific here) so load the server
+  console.log("Starting as Server");
   require('./server/server.js');
 }

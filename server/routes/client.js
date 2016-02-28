5698213863f273223618fbbb61192fcd58f7abc3
Philip Hutchins <philip@yikyakapp.com>
2016-02-28 11:39:26 -0500

    Fixing case sensitivity issue for linux

--- server/routes/client.js
+++ server/routes/client.js
@@ -4,8 +4,17 @@ module.exports = function(app, pipo) {
 
   app.get('/', function(req, res) {
     var username = 'default';
+
     res.render('preDeps.jade', { depRoot: '' }, function(err, preDeps) {
+      if (err) { return console.log("Failed to render preDeps: " + err); }
+
+      var preDeps = preDeps;
+
       res.render('postDeps.jade', { depRoot: '' }, function(err, postDeps) {
+        if (err) { return console.log("Failed to render postDeps: " + err); }
+
+        var postDeps = postDeps;
+
         res.render('client.jade', {
           username : username,
           preDeps: preDeps,
@@ -17,8 +26,13 @@ module.exports = function(app, pipo) {
 
   app.get('/:username', function(req, res) {
     var username = req.param('username');
+
     res.render('preDeps.jade', { depRoot: '' }, function(err, preDeps) {
+      if (err) { return console.log("Failed to render preDeps: " + err); }
+
       res.render('postDeps.jade', { depRoot: '' }, function(err, postDeps) {
+        if (err) { return console.log("Failed to render postDeps: " + err); }
+
         res.render('client.jade', {
           username : username,
           preDeps: preDeps,
